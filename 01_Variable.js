59996ce628a4b755013bc13f3b1f18fb697274c3
Rupali909 <rupalimore019@gmail.com>
2023-09-20 19:37:56 +0530

    Basic JS/Assignment

---
+++ 01_Variable.js
@@ -0,0 +1,31 @@
+var firstName; // variable declaration
+
+firstName = "Rupali";  // Variable initialization
+
+console.log(firstName);
+
+firstName = "Ms.Rupali"; // Variable update
+
+console.log(firstName);
+
+ 
+
+ 
+
+var lastName = "More Patil"; // variable declaration and initialization
+
+console.log(lastName);
+
+ 
+
+var height; // Variable Declaration
+
+console.log(height);
+
+height = "6"; // initialization
+
+console.log(height);
+
+height = "6.2 feet";// update
+
+console.log(height);
---
+++ 01_variableAssign.js
@@ -0,0 +1,43 @@
+
+var Fullname;
+
+
+console.log(Fullname);
+
+Fullname ="Rupali More";
+
+console.log(Fullname);
+
+Fullname = "Rupali Uttamrao More"
+
+console.log(Fullname);
+
+var myLove = "I Love JS"
+
+console.log(myLove);
+
+var firstName ="firstName-Rupali"
+console.log(firstName);
+
+var lastName ="lastName-More"
+console.log(lastName);
+
+var age ="age-25"
+console.log(age);
+
+var address ="Address-Parli vaijnath"
+console.log(address);
+
+var pincode ="Pincode-431515"
+console.log(pincode);
+
+var Gender ="Gender-Female"
+console.log(Gender);
+
+var Married ="Maritial status-Single"
+console.log(Married);
+
+
+
+
+
